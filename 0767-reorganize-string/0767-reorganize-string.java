0395c8a9a5c83b1186c84aa182255d960e8ded07
M-Harish6374 <149258070+M-Harish6374@users.noreply.github.com>
2024-08-15 20:44:55 +0530

    Time: 1 ms (93.16%), Space: 41.2 MB (91.27%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,35 +1,40 @@
 class Solution {
-    public String reorganizeString(String str) {
-       int[] hash = new int[26];
-    for (int i = 0; i < str.length(); i++) hash[str.charAt(i) - 'a']++;
+    public String reorganizeString(String s) {
+        int[] hash=new int[26];
+        for(int i=0;i<s.length();i++){
+            hash[s.charAt(i)-'a']++;
+        }
+        int max=0;
+        int length=0;
+        for(int i=0;i<hash.length;i++){
+            if(hash[i]>max){
+                max=hash[i];
+                length=i;
+            }
+        }
+        if(max>(s.length()+1)/2){
+            return "";
+        }
+        char[] arr= new char[s.length()];
+        int idx=0;
+      
+        while(hash[length]-- > 0){
+            arr[idx]=(char)(length+'a');
+            idx+=2;
+        }
+        for(int i=0;i<hash.length;i++){
+            while(hash[i]-- >0){
+                if(idx>=arr.length){
+                    idx=1;
 
-    int max = 0, letter = 0;
-    for (int i = 0; i < hash.length; i++)
-      if (hash[i] > max) {
-        max = hash[i];
-        letter = i;
-      }
+                }
+                arr[idx]=(char)(i+'a');
+                idx+=2;
+            }
+        }
 
-    if (max > (str.length() + 1) / 2) return "";
 
-    char[] res = new char[str.length()];
-
-    // Fill all even places with majority character
-    int idx = 0;
-    while (hash[letter]-- > 0) {
-      res[idx] = (char) (letter + 'a');
-      idx += 2;
-    }
-
-    // Fill the remaining characters
-    for (int i = 0; i < hash.length; i++)
-      while (hash[i]-- > 0) {
-        if (idx >= res.length) idx = 1;
-
-        res[idx] = (char) (i + 'a');
-        idx += 2;
-      }
-
-    return String.valueOf(res);
+        // String str= String.valueOf(arr);
+        return String.valueOf(arr);
     }
 }
