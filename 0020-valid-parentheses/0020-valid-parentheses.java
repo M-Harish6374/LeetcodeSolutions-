74c52e0e6294542262060b1f8b638f412822d7c8
M-Harish6374 <149258070+M-Harish6374@users.noreply.github.com>
2024-08-20 23:15:04 +0530

    Time: 49 ms (5.1%), Space: 44.9 MB (5.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,17 @@
-class Solution {
-    public boolean isValid(String s) {
-        char[] ch=s.toCharArray();
-        int count0=0,count1=0,count2=0,count3=0,count4=0,count5=0;
-        for(int i=0;i<ch.length;i++){
-        if(ch[i]=='('){
-            count0++;
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                // If the string becomes empty, it indicates all brackets are matched.
+                return s.isEmpty();
+            }
         }
-        else if(ch[i]=='{'){
-            count1++;
-        }
-        else if(ch[i]=='['){
-            count2++;
-        }
-        else if(ch[i]==')'){
-            count3++;
-        }
-        else if(ch[i]=='}'){
-            count4++;
-        }
-        else if(ch[i]==']'){
-            count5++;
-        }
-        }
-        if(count0==count3){
-            return true;
-        }
-        // if(count1==count4){
-        //     return true;
-        // }
-        // if(count2==count5){
-        //     return true;
-        // }
-        
-
-    
-    return false;
+       
     }
 }
