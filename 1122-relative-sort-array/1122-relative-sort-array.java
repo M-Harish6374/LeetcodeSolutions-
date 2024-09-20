704d44b4ebfd3b7a1f1cb62c46b421d3836ccd3c
M-Harish6374 <149258070+M-Harish6374@users.noreply.github.com>
2024-09-20 14:21:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] arr=new int[arr1.length];
+        int k=0;
+        for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<arr1.length-1;j++){
+                if(arr2[i]==arr1[j]){
+                    arr[k]=arr1[j];
+                    k++;
+                }
+            }
+        }
+        List<Integer> l1=new ArrayList<>();
+
+        for(int i=0;i<arr1.length;i++){
+            int count=0;
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    count++;
+                }
+                
+            }
+            if(count==0){
+                l1.add(arr1[i]);
+            }
+        }
+        Collections.sort(l1);
+        int l=0;
+        for(int i=arr1.length-l1.size();i<arr1.length;i++){
+            arr[i]=l1.get(l++);
+        }
+        System.out.print(l1);
+        return arr;
+    }
+}
